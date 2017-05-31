84ad0612f22acfcb215e7f892bb96cae10db1ce6
Amy Beall <amylouisebeall@gmail.com>
2017-05-30 18:21:18 -0700

    added code to auth

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ var authTokens = require('./authentication');
 Request.get('https://slack.com/oauth/authorize?client_id='+authTokens.client_id+'&scope='+authTokens.scope, function(error){
 	if(error)console.log(error);
 });
+App.get('/', function(req, res) {
+	authResp = res.json;
+	if(authResp.query.code !== ''){
+		var authCode = authResp.query.code;
+		Request.get('https://slack.com/api/oauth.access?client_id='+authTokens.client_id+'&client_secret='+authTokens.client_secret+'&code='+authCode, function(error, response, body){
+			if(error)console.log(error);
+			if(body)console.log(body);
+		});
+	}
+	res.sendFile(Path.join(__dirname,'README.md'));
+});
 
 // Accepts the post request from Slack
 Router.post('/lunch_tables', function(req, res) {  		
@@ -19,10 +30,7 @@ Router.post('/lunch_tables', function(req, res) {  
 // apply the routes to our application
 App.use('/', Router);
 
-App.get('/', function(req, res) {
-	console.log(req);
-	res.sendFile(Path.join(__dirname,'index.html'));
-});
+
 
 App.listen(8080, "");
 console.log('Server running at http://172.31.13.193:8080/');
